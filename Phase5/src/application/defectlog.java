fbbb69fe3b3903a71e1ad3696c1361173313794d
A7md9 <105965761+A7md9@users.noreply.github.com>
2023-04-28 13:11:47 -0700

    Create defectlog.java

---
+++ Phase5/src/application/defectlog.java
@@ -0,0 +1,44 @@
+# Ahmed Alyahya
+public class DefectLog {
+    private String projectType;
+    private String defectType;
+    private String defectDetails;
+    private String userName;
+
+    public DefectLog(String projectType, String defectType, String defectDetails, String userName) {
+        this.projectType = projectType;
+        this.defectType = defectType;
+        this.defectDetails = defectDetails;
+        this.userName = userName;
+    }
+
+    public String getProjectType() {
+        return projectType;
+    }
+
+    public String getDefectType() {
+        return defectType;
+    }
+
+    public String getDefectDetails() {
+        return defectDetails;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setDefectDetails(String defectDetails) {
+        this.defectDetails = defectDetails;
+    }
+
+    @Override
+    public String toString() {
+        return "DefectLog{" +
+                "projectType='" + projectType + '\'' +
+                ", defectType='" + defectType + '\'' +
+                ", defectDetails='" + defectDetails + '\'' +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
+}
