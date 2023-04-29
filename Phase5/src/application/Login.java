3ae62d9358b4b37b60fa5bfe6d6485a914eb13c8
A7md9 <105965761+A7md9@users.noreply.github.com>
2023-04-29 13:19:56 -0700

    Update Login.java
    added the password attempts

--- Phase5/src/application/Login.java
+++ Phase5/src/application/Login.java
@@ -1,4 +1,5 @@
 ///Author: Sishir Vallurupalli
+///Author: Sishir Vallurupalli
 package application;
 
 import javafx.application.Application;
@@ -16,62 +17,72 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Login {
-	public Login()
-	{
-		
-	}
-	
-	@FXML
-	private Button button;
-	
-	@FXML 
-	private Label wrongLogIn;
-	
-	@FXML
-	private TextField username;
-	
-	@FXML
-	private TextField passwordText;
-	
-	@FXML
-	private Button resetPassword;
-	
-	@FXML
-	private Button signUp;
-	
-	
-	public void login(ActionEvent event) throws IOException{
-		Main m = new Main();
-		
-		ArrayList<UserInfo> users = m.getUsers();
-		
-		String user = username.getText().toString();
-		String pass = passwordText.getText().toString();
-		for (int x = 0; x < users.size(); x++)
-		{
-			if (users.get(x).getUserName().equals(user))
-			{
-				if (users.get(x).getPassword().equals(pass))
-				{
-					m.setCurrentUser(users.get(x));
-					m.changeScene("Menu.fxml");
-					break;
-				}
-			}
-		}
-		wrongLogIn.setText("Wrong Log In Information");
-		
-		
-	}
-	
-	public void ResetPassword(ActionEvent event) throws IOException{
-		Main m = new Main();
-		m.changeScene("ResetPassword.fxml");
-	}
-	
-	public void SignUp(ActionEvent event) throws IOException{
-		Main m = new Main();
-		m.changeScene("SignUp.fxml");
-	}
+    public Login() {
+    }
 
+    @FXML
+    private Button button;
+
+    @FXML
+    private Label wrongLogIn;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField passwordText;
+
+    @FXML
+    private Button resetPassword;
+
+    @FXML
+    private Button signUp;
+
+    private int attempts = 0;
+
+    public void login(ActionEvent event) throws IOException {
+        if (attempts >= 3) {
+            wrongLogIn.setText("Too many attempts. Please try again later.");
+            button.setDisable(true);
+            return;
+        }
+
+        Main m = new Main();
+
+        ArrayList<UserInfo> users = m.getUsers();
+
+        String user = username.getText().toString();
+        String pass = passwordText.getText().toString();
+        boolean loggedIn = false;
+
+        for (int x = 0; x < users.size(); x++) {
+            if (users.get(x).getUserName().equals(user)) {
+                if (users.get(x).getPassword().equals(pass)) {
+                    m.setCurrentUser(users.get(x));
+                    m.changeScene("Menu.fxml");
+                    loggedIn = true;
+                    break;
+                }
+            }
+        }
+
+        if (!loggedIn) {
+            wrongLogIn.setText("Wrong Log In Information");
+            attempts++;
+            if (attempts >= 3) {
+                wrongLogIn.setText("Too many attempts. Please try again later.");
+                button.setDisable(true);
+            }
+        }
+    }
+
+    public void ResetPassword(ActionEvent event) throws IOException {
+        Main m = new Main();
+        m.changeScene("ResetPassword.fxml");
+    }
+
+    public void SignUp(ActionEvent event) throws IOException {
+        Main m = new Main();
+        m.changeScene("SignUp.fxml");
+    }
 }
